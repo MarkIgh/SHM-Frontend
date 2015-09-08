e25b9844c109de50b0be43d1540786dc18105ef4
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-08 23:32:20 +0300

    404 500 errors handler

--- dev/src/js/controllers/errors/httpget.js
+++ dev/src/js/controllers/errors/httpget.js
@@ -8,6 +8,18 @@ function errors_httpget(data, status, headers, config) {
       type: "error",
       showLoaderOnConfirm: true, },
     function(){ window.location.href="../Signin"; });
+  }else if (status == 404) {
+      swal({
+      title:"Not found",
+      text: "Seems that api handler or file that you are looking not found at server.",
+      type: "error",
+      showLoaderOnConfirm: true, });
+  }else if (status == 500) {
+      swal({
+      title:"Server error",
+      text: "Server responsed 500 code. Something goes wrong.",
+      type: "error",
+      showLoaderOnConfirm: true, });
   }else{
     sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
     "error");
