2075566c523c80dee5b1c273878e3e41edefd8cf
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-08 23:24:27 +0300

    session errors removed

--- dev/src/js/controllers/SessionInfo.js
+++ dev/src/js/controllers/SessionInfo.js
@@ -1,6 +1,5 @@
 app.controller('SessionInfo', ['$scope', '$filter', '$http',
   function($scope, $filter, $http, $location){
-    errors_httpget(data, status, headers);
     // Get Session Info function
     $scope.getInfo=function(){
         $http.get('../api/info/session').
