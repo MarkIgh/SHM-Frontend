5e45fc4e9318707220042797192edcf10306c22d
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-08 23:20:00 +0300

    session info

--- dev/src/js/controllers/SessionInfo.js
+++ dev/src/js/controllers/SessionInfo.js
@@ -10,7 +10,20 @@ app.controller('SessionInfo', ['$scope', '$filter', '$http',
           }
         ).
         // Errors handling
-        error();
+        error(function(data, status, headers,config){
+          // If session expired
+          if (status == 401) {
+            swal({
+              title:"Session expired",
+              text: "Your session has expired or not found. Please relogin.",
+              type: "error",
+              showLoaderOnConfirm: true, },
+            function(){ window.location.href="../Signin"; });
+          }else{
+            sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
+            "error");
+          };
+        });
     };
     // Run GetInfo to get session info
     $scope.getInfo();
