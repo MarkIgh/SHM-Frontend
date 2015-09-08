10b0b5022c082b7039f5faac7370617ed2b499e6
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-08 22:16:25 +0300

    http get error handler added

--- dev/src/js/controllers/SessionInfo.js
+++ dev/src/js/controllers/SessionInfo.js
@@ -9,21 +9,9 @@ app.controller('SessionInfo', ['$scope', '$filter', '$http',
             $scope.sessionInfo = data;
           }
         ).
-        error(function(data, status, headers, config) {
-          if (status == 401) {
-            swal({
-              title: "Session expired",
-              text: "Your session expired or not found. Plese relogin.",
-              type: "error",
-              showLoaderOnConfirm: true, },
-            function(){ window.location.href="../Signin"; });
-          }else{
-            sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
-            "error");
-          };
-        });
+        // Errors handling
+        error(errors_httpget(data, status, headers));
     };
     // Run GetInfo to get session info
     $scope.getInfo();
-
 }]);
