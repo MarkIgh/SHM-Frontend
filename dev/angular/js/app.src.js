e4d1901a632dcef3c0c020b93bdd0a34bf5cff28
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-13 23:11:00 +0300

    T

--- dev/angular/js/app.src.js
+++ dev/angular/js/app.src.js
@@ -77595,8 +77595,8 @@ angular.module('app')
   }]);
 // Erros handling during http.Get requests to server's API
 function httpGetError(data, status) {
-        // If session expired
         switch (status) {
+        // Session expired
         case 401:
               swal({
                 title:"Session expired",
@@ -77605,6 +77605,7 @@ function httpGetError(data, status) {
                 showLoaderOnConfirm: true, },
           function(){ window.location.href="../Signin"; });
                 break;
+        // Acces to api module denied
         case 403:
             swal({
                 title:"Access denied",
@@ -77612,6 +77613,7 @@ function httpGetError(data, status) {
                 type: "error",
                 showLoaderOnConfirm: true, });
             break;
+        // Not found
         case 404:
             swal({
                 title:"Not found",
@@ -77619,23 +77621,23 @@ function httpGetError(data, status) {
                 type: "error",
                 showLoaderOnConfirm: true, });
             break;
+        // Server error
         case 500:
+            // Check for panicerr resopnse
+            if (data.error.code != null){
+                swal({
+                    title:"Wrong request: "+ data.error.code,
+                    text: "Server return error, because wrong request type: "+data.error.text,
+                    type: "error",
+                    showLoaderOnConfirm: true, });
+            }else{
+            // If no pnicerr response
+              sweetAlert("Server error", "Something went wrong! Server response code: "+status,
+              "error");
+            }
+            break;
 
-                try{
-                      resp=JSON.parse(data);
-                      alert(data);
-                      alert(resp);
-                      if (resp.error.code != null){
-
-                      }else{
-
-                      }
-                }catch(e){
-                    alert('Data: '+data.error.code);
-                    alert('Json parse error: '+e);
-                };
-                break;
-
+        // Something wrong
         default:
             sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
             "error");
