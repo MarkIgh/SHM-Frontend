eb979b2b236fd4e405af1599ac99a9f3f5f008ca
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-13 20:22:32 +0300

    T

--- dev/src/js/controllers/SessionInfo.js
+++ dev/src/js/controllers/SessionInfo.js
@@ -1,4 +1,4 @@
-app.controller('SessionInfo', ['$scope', '$filter', '$http', 'ErrorHandler'
+app.controller('SessionInfo', ['$scope', '$filter', '$http', 'ErrorHandler',
   function($scope, $filter, $http, $location, ErrorHandler){
     // Get Session Info function
     $scope.getInfo=function(){
