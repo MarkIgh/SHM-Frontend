1c67cb1125a8a403b2ae9509269ee2c07fdb8501
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-13 22:45:48 +0300

    T

--- dev/src/js/controllers/errors/http.js
+++ dev/src/js/controllers/errors/http.js
@@ -1,48 +1,54 @@
 // Erros handling during http.Get requests to server's API
 function httpGetError(data, status) {
         // If session expired
-        if (status == 401) {
+        switch (status) {
+        case 401:
               swal({
                 title:"Session expired",
                 text: "Your session has expired or not found. Please relogin.",
                 type: "error",
                 showLoaderOnConfirm: true, },
           function(){ window.location.href="../Signin"; });
-        }else if (status == 403) {
+                break;
+        case 403:
             swal({
                 title:"Access denied",
                 text: "Request is forbidden by your account configuration.",
                 type: "error",
                 showLoaderOnConfirm: true, });
-        }else if (status == 404) {
+            break;
+        case 404:
             swal({
                 title:"Not found",
                 text: "Seems that api handler or file that you are looking not found at server.",
                 type: "error",
                 showLoaderOnConfirm: true, });
-        }else if (status == 500) {
+            break;
+        case 500:
 
                 try{
 
                     resp=JSON.parse(data);
                     if (resp.error != null){
                         swal({
-                            title:"Wrong request:"+ resp.error.code,
+                            title:"Wrong request: "+ resp.error.code,
                             text: "Server return error, because wrong request type: "+resp.error.text,
                             type: "error",
                             showLoaderOnConfirm: true, });
                     }else{
-                      sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
+                      sweetAlert("Server error", "Something went wrong! Server response code: "+status,
                       "error");
                     }
 
                 }catch(e){
-                    sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
+                    sweetAlert("Server error", "Something went wrong! Server response code: "+status,
                     "error");
                 };
+                break;
 
-        }else{
+        default:
             sweetAlert("Unknown server response", "Something went wrong! Server response code: "+status,
             "error");
-        };
+            break;
+       };
 };
