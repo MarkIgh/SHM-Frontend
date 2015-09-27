7346e7ee2a9c864103762622bcfc0b3f511e7eaf
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-27 21:00:30 +0300

    T

--- dev/src/js/controllers/LiveStats.js
+++ dev/src/js/controllers/LiveStats.js
@@ -1,8 +1,8 @@
 app.controller('LiveStats', ['$scope', '$filter', '$http',
     function($scope, $filter, $http){
 
-    $scope.$apply(function() {
+
         $scope.Stats.CPU = 20;
-    });
+
 
 }]);
