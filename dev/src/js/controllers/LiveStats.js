b9f01fed893ddd99c4b20def54044b888c041ce9
kalashnikovm@mail.ru <kalashnikovm@mail.ru>
2015-09-27 20:57:01 +0300

    T

--- dev/src/js/controllers/LiveStats.js
+++ dev/src/js/controllers/LiveStats.js
@@ -1,20 +1,8 @@
 app.controller('LiveStats', ['$scope', '$filter', '$http',
     function($scope, $filter, $http){
 
-      var ws = new WebSocket("wss://93.183.203.13:10443/api/core/livesysstat");
-
-      ws.onopen = function(){
-        console.log("Socket for livestats has been opened!");
-      };
-
-      ws.onmessage = function(message, $scope, $rootScope) {
-        console.log(JSON.parse(message.data));
-
-        var obj = JSON.parse(message.data);
-        $scope.$apply(function ($scope) {
-            $scope.Stats = {};
-            $scope.Stats = obj;
-        });
-      };
+    $scope.$apply(function() {
+        $scope.Stats.CPU = 20;
+    });
 
 }]);
