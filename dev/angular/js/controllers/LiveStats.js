8e33c75f876c0f055f61b2198ed2b3247136a243
Kalashnikov Max <kalashnikovm@mail.ru>
2016-04-26 17:26:22 +0300

    fix default sort

--- dev/angular/js/controllers/LiveStats.js
+++ dev/angular/js/controllers/LiveStats.js
@@ -8,19 +8,18 @@ app.controller('LiveStats', ['$scope', '$filter', '$http',
       };
 
       ws.onmessage = function(message) {
-       
-        $scope.$apply(function() {
             // Update scope
             $scope.Stats = JSON.parse(message.data);
             $scope.sortType = 'CPU';
+            $scope.sortReverse = false;
+            
             $scope.$digest;
             // Update PIE charts
             $('#PChartCPU').data('easyPieChart').update($scope.Stats.CPU);
             $('#PChartRAM').data('easyPieChart').update($scope.Stats.RAM);
             $('#PChartSWAP').data('easyPieChart').update($scope.Stats.SWAP);
             $('#PChartWA').data('easyPieChart').update($scope.Stats.WA);
-            
-        });
+
       };
 
 }]);
