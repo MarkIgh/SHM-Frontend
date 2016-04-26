2c83206767a85b0b0e089a01e719aed62153ebf4
Kalashnikov Max <kalashnikovm@mail.ru>
2016-04-26 22:24:45 +0300

    fix@11

--- dev/angular/js/services/SessionInfo.js
+++ dev/angular/js/services/SessionInfo.js
@@ -13,6 +13,17 @@ app.service('SessionInfo', function ($http) {
         });
         
     this.Get = function () {
+         $http.get('../api/info/session').
+        success(function(data, status, headers, config) {
+            // Set the data
+            this.Info = data;
+        }
+        ).
+        // Errors handling
+        error(function(data, status){
+            httpGetError(data, status);
+        });
+        
         alert('Username:'+this.Info.Username);
         return this.Info;
     };
