809adafc6c0384615f8ba1f072f5c8261e8902bf
Kalashnikov Max <kalashnikovm@mail.ru>
2016-04-26 23:10:47 +0300

    fix#19

--- dev/angular/js/app.src.js
+++ dev/angular/js/app.src.js
@@ -75929,20 +75929,20 @@ app.service('SessionInfo', function ($http) {
     this.Info = {};
     
     this.Get = function() { 
-         $http.get('../api/info/session').
-            success(function(data, status, headers, config) {
-            // Set the data
-            this.Info = data;
-            console.log('sessinfo'+JSON.stringify(data));
-            console.log('sessinfo'+JSON.stringify(this.Info));
-            return this.Info;
-        }
-        ).
-        // Errors handling
-        error(function(data, status){
-            httpGetError(data, status);
+
+      if ( !promise ) {
+        // $http returns a promise, which has a then function, which also returns a promise
+        promise = $http.get('../api/info/session').then(function (response) {
+          // The then function here is an opportunity to modify the response
+          console.log(response);
+          this.Info = response.data;
+          // The return value gets picked up by the then in the controller.
+          return this.Info;
         });
-    };
+      }
+      // Return the promise to the controller
+      return promise;
+    }
 });
 ;(function(window, document, undefined) {
   "use strict";
