759d3aa0afb66d7b94d54337c1328b9b243ac88e
Maksim Kalashnikov <kalashnikovm@mail.ru>
2016-06-17 14:54:13 +0300

    #8

--- dev/angular/js/Slack.js
+++ dev/angular/js/Slack.js
@@ -3,4 +3,3 @@ function Slack_OnLoad(Plugins)
     alert("Slack on load, Plugin list:"+Plugins.getHTMLInjectors);
 }
 
-Slack_OnLoad(Plugins);
--- dev/angular/js/app.src.js
+++ dev/angular/js/app.src.js
@@ -74722,7 +74722,6 @@ function Slack_OnLoad(Plugins)
     alert("Slack on load, Plugin list:"+Plugins.getHTMLInjectors);
 }
 
-Slack_OnLoad(Plugins);
 
 'use strict';
 
